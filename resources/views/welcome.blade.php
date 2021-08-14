106b42d93352d030562260eedc7782620e98e9d8
sedorutt <trustpenguin@gmail.com>
2021-08-15 00:35:44 +0900

    eigth

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        {{-- <meta name="csrf-token" content="{{ csrf_token() }}"> --}}
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>Laravel</title>
 
@@ -26,6 +26,6 @@
         <p>hello</p>
         <router-view></router-view>
     </div>
-    <script src="{{ asset('/js/app.js') }}" defer></script>
+    <script src="../js/app.js" defer></script>
     </body>
 </html>
