d31ff6c3c943766e90169026d5b9c13882a8ff3e
sedorutt <trustpenguin@gmail.com>
2021-08-17 13:29:47 +0900

    script

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -26,7 +26,40 @@
         <p>hello</p>
         <router-view></router-view>
     </div>
-    <script type="text/javascript" src="file:///Users/naokiaruga/Desktop/dev/todo/routes/api.php" defer></script>
+    <script type="text/javascript" src="{{mix('js/app.js')}}" defer>
+        import Vue from 'vue';
+        import VueRouter from 'vue-router';
+        import TodoListComponent from './components/TodoListComponent.vue';
+
+
+        require('./bootstrap');
+
+        window.Vue = require('vue').default;
+        Vue.use(VueRouter);                         //* これを忘れない！！
+
+
+        Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+
+
+        const router = new VueRouter({
+            mode: 'history',
+            routes: [
+                {
+                    path: '/',
+                    name: 'Todo',
+                    component: TodoListComponent
+                },
+            ]
+        })
+
+        const app = new Vue({
+            el: '#app',
+            router:router
+        });
+
+
+    </script>
     
     </body>
 </html>
